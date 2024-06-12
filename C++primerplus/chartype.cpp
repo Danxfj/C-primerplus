e8e769027ce99e1808b92decc494a1d8e2c58b9e
Lockey <fxiao029@gmail.com>
2024-06-12 21:59:29 +0800

    2024612

---
+++ C++primerplus/chartype.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char ch;
+
+	cout << "please cin for the char: ";
+	cin >> ch;
+	cout << "Hola!";
+	cout << "Thank you for the character " << ch << " cin\n";
+	return 0;
+}
